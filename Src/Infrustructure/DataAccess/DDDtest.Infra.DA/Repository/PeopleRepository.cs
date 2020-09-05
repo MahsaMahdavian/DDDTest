c8dafdadd7178e9605dfccf6889e3d69c5edbc25
mahsamahdavian <mahsaa.mahdaviaan@gmail.com>
2020-09-06 00:01:56 +0430

    AddUntrackedFiles

---
+++ Src/Infrustructure/DataAccess/DDDtest.Infra.DA/Repository/PeopleRepository.cs
@@ -0,0 +1,16 @@
+﻿using DDDTest.Domain.Contract.Repository;
+using DDDTest.Domain.Person;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDtest.Infra.DA.Repository
+{
+    public class PeopleRepository : BaseRepository<PersonModel, PeopleContext>, IPeopleRepository
+    {
+        public PeopleRepository(PeopleContext Context) : base(Context)
+        {
+        }
+    }
+}
